dc7b85b2dcbe122f17b5576636a953a3d16481de
Romans Malinovskis <me@nearly.guru>
2024-04-11 17:54:24 +0100

    added sqlite binding

--- src/traits/datasource.rs
+++ src/traits/datasource.rs
@@ -4,6 +4,6 @@ use std::result::Result;
 use crate::Query;
 
 pub trait DataSource {
-    fn query_fetch(&self, query: Query) -> Result<Vec<Vec<String>>, Box<dyn Error>>;
-    fn query_exec(&self, query: Query) -> Result<(), Box<dyn Error>>;
+    fn query_fetch(&self, query: &Query) -> Result<Vec<Vec<String>>, Box<dyn Error>>;
+    fn query_exec(&self, query: &Query) -> Result<(), Box<dyn Error>>;
 }
