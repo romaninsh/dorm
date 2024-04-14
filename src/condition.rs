f58b721cc7532246f019684e9d699eae17d1601f
Romans Malinovskis <me@nearly.guru>
2024-04-14 23:11:00 +0100

    added conditions

---
+++ src/condition.rs
@@ -0,0 +1,51 @@
+use std::ops::Deref;
+
+use crate::Renderable;
+
+pub struct Condition {
+    field: &'static str,
+    operation: &'static str,
+    value: String,
+}
+
+impl Condition {
+    pub fn new(field: &'static str, operation: &'static str, value: String) -> Condition {
+        Condition {
+            field,
+            operation,
+            value,
+        }
+    }
+}
+
+impl Deref for Condition {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        &self.field
+    }
+}
+
+impl<'a> Renderable<'a> for Condition {
+    fn render(&self) -> String {
+        format!("{} {} '{}'", self.field, self.operation, self.value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_condition() {
+        let field = "id";
+
+        let condition = Condition {
+            field: &field,
+            operation: "=",
+            value: "1".to_string(),
+        };
+
+        assert_eq!(condition.render(), "id = '1'");
+    }
+}
